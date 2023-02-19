bf2db2b7069d67f5ddcdec7f310fb5ca13ede028
Azamat <t.azamat@dogovor24.kz>
2023-02-19 23:27:50 +0600

    created model, handler

---
+++ api/handlers/register.go
@@ -0,0 +1,17 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func registerHandler(c *gin.Context) {
+	var user User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "User registered"})
+}
---
+++ api/models/user.go
@@ -0,0 +1,8 @@
+package models
+
+type User struct {
+	Name     string `json:"name"`
+	Surname  string `json:"surname"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
